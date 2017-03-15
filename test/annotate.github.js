be2f43cb663218a995f435441657b7a4ea284dcb
--global <aymen.mouelhi@gmail.com>
2017-03-15 22:00:37 +0100

    handle chalk background

--- test/annotate.github.js
+++ test/annotate.github.js
@@ -1,10 +1,10 @@
-// NOTE: This is the sample output for a note!
-// OPTIMIZE (aymen mouelhi): This is the sample output for an optimize with an author!
-// TODO: This is the sample output for a todo!
-// HACK: This is the sample output for a hack! Don't commit hacks!
-// XXX: This is the sample output for a XXX! XXX's need attention too!
-// FIXME (aymen mouelhi): This is the sample output for a fixme! Seriously fix this...
-// BUG: This is the sample output for a bug! Who checked in a bug?!
+// NOTE: This is the sample output for a note! [Issue #92]
+// OPTIMIZE (aymen mouelhi): This is the sample output for an optimize with an author! [Issue #94]
+// TODO: This is the sample output for a todo! [Issue #97]
+// HACK: This is the sample output for a hack! Don't commit hacks! [Issue #91]
+// XXX: This is the sample output for a XXX! XXX's need attention too! [Issue #100]
+// FIXME (aymen mouelhi): This is the sample output for a fixme! Seriously fix this... [Issue #96]
+// BUG: This is the sample output for a bug! Who checked in a bug?! [Issue #88]
 
 require('../lib/')({
   path:                 process.cwd(),
